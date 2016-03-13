2259a23fce2b7c60fe7f6c693cc90a3aac9b79f0
benstevens <stevens.benjaminl@gmail.com>
2016-03-12 22:19:49 -0500

    refactored some code

--- core/src/edu/cornell/gdiac/physics/fish/FishController.java
+++ core/src/edu/cornell/gdiac/physics/fish/FishController.java
@@ -1,5 +1,5 @@
 /*
- * Fish.java
+ * FishController.java
  *
  * Author: Walker M. White && Dashiell Brown
  * Based on original PhysicsDemo Lab by Don Holden, 2007
@@ -22,7 +22,7 @@ import edu.cornell.gdiac.physics.*;
 import edu.cornell.gdiac.physics.obstacle.*;
 
 /**
- * Gameplay specific controller for the rocket lander game.
+ * Gameplay specific controller for Downstream.
  *
  * You will notice that asset loading is not done with static methods this time.  
  * Instance asset loading makes it easier to process our game modes in a loop, which 
@@ -32,38 +32,37 @@ import edu.cornell.gdiac.physics.obstacle.*;
  */
 public class FishController extends WorldController implements ContactListener {
 	/** Reference to the fish texture */
-	private static final String ROCK_TEXTURE = "rocket/fish3.png";
+	private static final String KOI_TEXTURE = "fish/fish3.png";
 	/** The reference for the tether textures  */
-	private static final String TETHER_TEXTURE = "rocket/lilipad.png";
-	/** Reference to the crate image assets */
-	private static final String CRATE_PREF = "rocket/crate0";
-	/** How many crate assets we have */
-	private static final int MAX_CRATES = 2;
+	private static final String TETHER_TEXTURE = "fish/lilypad.png";
+	/** Reference to the enemy image assets */
+	private static final String ENEMY_TEXTURE = "fish/enemy.png";
 
 	/** The asset for the collision sound */
-	private static final String  COLLISION_SOUND = "rocket/bump.mp3";
+	private static final String  COLLISION_SOUND = "fish/bump.mp3";
 	/** The asset for the main afterburner sound */
-	private static final String  MAIN_FIRE_SOUND = "rocket/afterburner.mp3";
+	private static final String  MAIN_FIRE_SOUND = "fish/afterburner.mp3";
 	/** The asset for the right afterburner sound */
-	private static final String  RGHT_FIRE_SOUND = "rocket/sideburner-right.mp3";
+	private static final String  RGHT_FIRE_SOUND = "fish/sideburner-right.mp3";
 	/** The asset for the left afterburner sound */
-	private static final String  LEFT_FIRE_SOUND = "rocket/sideburner-left.mp3";
+	private static final String  LEFT_FIRE_SOUND = "fish/sideburner-left.mp3";
 	
-	/** Texture assets for the rocket */
-	private TextureRegion rocketTexture;
-	/** Texture assets for the lilipads */
+	/** Texture assets for the koi */
+	private TextureRegion koiTexture;
+	/** Texture assets for the lilypads */
 	private TextureRegion tetherTexture;
+	/** Texture assets for the enemy fish */
+	private TextureRegion enemyTexture;
+
 	/** Texture filmstrip for the main afterburner */
 	private FilmStrip mainTexture;
 	/** Texture filmstrip for the main afterburner */
 	private FilmStrip leftTexture;
 	/** Texture filmstrip for the main afterburner */
-	private FilmStrip rghtTexture;
+	private FilmStrip rightTexture;
 	
-	/** Texture assets for the crates */
-	private TextureRegion[] crateTextures = new TextureRegion[MAX_CRATES];
 	/** Track asset loading from all instances and subclasses */
-	private AssetState rocketAssetState = AssetState.EMPTY;
+	private AssetState fishAssetState = AssetState.EMPTY;
 	
 	private boolean tethered;
 	/**
@@ -77,19 +76,19 @@ public class FishController extends WorldController implements ContactListener {
 	 * @param manager Reference to global asset manager.
 	 */
 	public void preLoadContent(AssetManager manager) {
-		if (rocketAssetState != AssetState.EMPTY) {
+		if (fishAssetState != AssetState.EMPTY) {
 			return;
 		}
 		
-		rocketAssetState = AssetState.LOADING;
-		for (int ii = 0; ii < crateTextures.length; ii++) {
-			manager.load(CRATE_PREF + (ii + 1) +".png", Texture.class);
-			assets.add(CRATE_PREF + (ii + 1) +".png");
-		}
+		fishAssetState = AssetState.LOADING;
+
+		
+		manager.load(ENEMY_TEXTURE, Texture.class);
+		assets.add(ENEMY_TEXTURE);
 		
 		// Ship textures
-		manager.load(ROCK_TEXTURE, Texture.class);
-		assets.add(ROCK_TEXTURE);
+		manager.load(KOI_TEXTURE, Texture.class);
+		assets.add(KOI_TEXTURE);
 		
 		manager.load(TETHER_TEXTURE, Texture.class);
 		assets.add(TETHER_TEXTURE);
@@ -118,16 +117,12 @@ public class FishController extends WorldController implements ContactListener {
 	 * @param manager Reference to global asset manager.
 	 */
 	public void loadContent(AssetManager manager) {
-		if (rocketAssetState != AssetState.LOADING) {
+		if (fishAssetState != AssetState.LOADING) {
 			return;
 		}
-		
-		for (int ii = 0; ii < crateTextures.length; ii++) {
-			String filename = CRATE_PREF + (ii + 1) +".png";
-			crateTextures[ii] = createTexture(manager,filename,false);
-		}
-		
-		rocketTexture = createTexture(manager,ROCK_TEXTURE,false);
+
+		enemyTexture = createTexture(manager,ENEMY_TEXTURE,false);
+		koiTexture = createTexture(manager,KOI_TEXTURE,false);
 		tetherTexture = createTexture(manager,TETHER_TEXTURE,false);
 		
 		SoundController sounds = SoundController.getInstance();
@@ -137,25 +132,25 @@ public class FishController extends WorldController implements ContactListener {
 		sounds.allocate(manager,COLLISION_SOUND);
 		
 		super.loadContent(manager);
-		rocketAssetState = AssetState.COMPLETE;
+		fishAssetState = AssetState.COMPLETE;
 	}
 	
 	// Physics constants for initialization
-	/** Density of non-crate objects */
+	/** Density of non-enemy objects */
 	private static final float BASIC_DENSITY   = 0.0f;
-	/** Density of the crate objects */
-	private static final float CRATE_DENSITY   = 1.0f;
-	/** Friction of non-crate objects */
+	/** Density of the enemy objects */
+	private static final float ENEMY_DENSITY   = 1.0f;
+	/** Friction of non-enemy objects */
 	private static final float BASIC_FRICTION  = 0.1f;
-	/** Friction of the crate objects */
-	private static final float CRATE_FRICTION  = 0.3f;
+	/** Friction of the enemy objects */
+	private static final float ENEMY_FRICTION  = 0.3f;
 	/** Collision restitution for all objects */
 	private static final float BASIC_RESTITUTION = 0.1f;
 	/** Threshold for generating sound on collision */
 	private static final float SOUND_THRESHOLD = 1.0f;
 	
-	private static final float TETHER_DENSITY = CRATE_DENSITY;
-	private static final float TETHER_FRICTION = CRATE_FRICTION;
+	private static final float TETHER_DENSITY = ENEMY_DENSITY;
+	private static final float TETHER_FRICTION = ENEMY_FRICTION;
 	private static final float TETHER_RESTITUTION = BASIC_RESTITUTION;
 
 	// Since these appear only once, we do not care about the magic numbers.
@@ -188,23 +183,23 @@ public class FishController extends WorldController implements ContactListener {
 	private ArrayList<TetherModel> tethers = new ArrayList<TetherModel>();
 
 	// Other game objects
-	/** The initial rocket position */
-	private static Vector2 ROCK_POS = new Vector2(24, 4);
+	/** The initial koi position */
+	private static Vector2 KOI_POS = new Vector2(24, 4);
 	/** The goal door position */
 	private static Vector2 GOAL_POS = new Vector2( 6, 12);
 
 	// Physics objects for the game
 	/** Reference to the goalDoor (for collision detection) */
 	private BoxObstacle goalDoor;
-	/** Reference to the rocket/player avatar */
-	private PlayerFishModel fish;
+	/** Reference to the player avatar */
+	private PlayerFishModel koi;
 	
 	private EnemyFish eFish;
 
 	/**
-	 * Creates and initialize a new instance of the rocket lander game
+	 * Creates and initialize a new instance of Downstream
 	 *
-	 * The game has default gravity and other settings
+	 * The game has no  gravity and deafault settings
 	 */
 	public FishController() {
 		setDebug(false);
@@ -298,14 +293,14 @@ public class FishController extends WorldController implements ContactListener {
 		tethers.add(tether);
 		
 
-		TextureRegion texture = crateTextures[1];
+		TextureRegion texture = enemyTexture;
 		dwidth  = texture.getRegionWidth()/scale.x;
 		dheight = texture.getRegionHeight()/scale.y;
 		eFish = new EnemyFish(20, 0, dwidth, dheight);
-		eFish.setDensity(CRATE_DENSITY);
-		eFish.setFriction(CRATE_FRICTION);
+		eFish.setDensity(ENEMY_DENSITY);
+		eFish.setFriction(ENEMY_FRICTION);
 		eFish.setRestitution(BASIC_RESTITUTION);
-		eFish.setName("crate"+ 1);
+		eFish.setName("enemy");
 		eFish.setDrawScale(scale);
 		eFish.setTexture(texture);
 		eFish.setAngle((float) (Math.PI/2));
@@ -349,13 +344,14 @@ public class FishController extends WorldController implements ContactListener {
 
 
 		// Create the fish avatar
-		dwidth  = rocketTexture.getRegionWidth()/scale.x;
-		dheight = rocketTexture.getRegionHeight()/scale.y;
-		fish = new PlayerFishModel(ROCK_POS.x, ROCK_POS.y, dwidth, dheight);
-		fish.setDrawScale(scale);
-		fish.setTexture(rocketTexture);
+		dwidth  = koiTexture.getRegionWidth()/scale.x;
+		dheight = koiTexture.getRegionHeight()/scale.y;
+		koi = new PlayerFishModel(KOI_POS.x, KOI_POS.y, dwidth, dheight);
+		koi.setDrawScale(scale);
+		koi.setName("koi");
+		koi.setTexture(koiTexture);
 	  
-		addObject(fish);
+		addObject(koi);
 	}
 
 	/**
@@ -370,19 +366,19 @@ public class FishController extends WorldController implements ContactListener {
 	 */
 	public void update(float dt) {
 
-		float thrust = fish.getThrust();
+		float thrust = koi.getThrust();
 		InputController input = InputController.getInstance();
-		fish.setFX(thrust * input.getHorizontal());
-		fish.setFY(thrust * input.getVertical());
-		fish.applyForce();
-		fish.setLinearVelocity(fish.getLinearVelocity().setLength(8));
+		koi.setFX(thrust * input.getHorizontal());
+		koi.setFY(thrust * input.getVertical());
+		koi.applyForce();
+		koi.setLinearVelocity(koi.getLinearVelocity().setLength(8));
 		
 		if (input.didLaunch()) tethered = !tethered;
 		
 		TetherModel closestTether = tethers.get(0);
-		float closestDistance = tethers.get(0).getPosition().sub(fish.getPosition()).len2();
+		float closestDistance = tethers.get(0).getPosition().sub(koi.getPosition()).len2();
 		for (TetherModel tether : tethers) {
-			float newDistance = tether.getPosition().sub(fish.getPosition()).len2();
+			float newDistance = tether.getPosition().sub(koi.getPosition()).len2();
 			if (newDistance < closestDistance) {
 				closestDistance = newDistance;
 				closestTether = tether;
@@ -390,12 +386,12 @@ public class FishController extends WorldController implements ContactListener {
 		}
 //		if (tethered &&
 		if (input.space && 
-			fish.getPosition().sub(fish.getInitialTangentPoint(closestTether)).len2() < .1) {
-			fish.applyTetherForce(closestTether);			
+			koi.getPosition().sub(koi.getInitialTangentPoint(closestTether)).len2() < .1) {
+			koi.applyTetherForce(closestTether);			
 		}
 		
 		float angV = 3f;
-		float radius = closestTether.getPosition().dst(fish.getPosition());
+		float radius = closestTether.getPosition().dst(koi.getPosition());
 		float tetherSpeed = angV*radius;
 		
 		float MAX_SPEED = 7f;
@@ -406,18 +402,18 @@ public class FishController extends WorldController implements ContactListener {
 //		if (fish.getLinearVelocity().len2() != 0) {
 //			switch(motionType){
 //			case 0:
-//				fish.setLinearVelocity(fish.getLinearVelocity().setLength(MAX_SPEED));
+//				koi.setLinearVelocity(koi.getLinearVelocity().setLength(MAX_SPEED));
 //				break;
 //			case 1:
-//				if (fish.getLinearVelocity().len() <= MAX_SPEED - 1 && input.accel){
-//					fish.setLinearVelocity(fish.getLinearVelocity().setLength(fish.getLinearVelocity().len()+1));
+//				if (koi.getLinearVelocity().len() <= MAX_SPEED - 1 && input.accel){
+//					koi.setLinearVelocity(koi.getLinearVelocity().setLength(koi.getLinearVelocity().len()+1));
 //				}
-//				if (fish.getLinearVelocity().len() >= MIN_SPEED + 1 && input.deccel){
-//					fish.setLinearVelocity(fish.getLinearVelocity().setLength(fish.getLinearVelocity().len()-1));
+//				if (koi.getLinearVelocity().len() >= MIN_SPEED + 1 && input.deccel){
+//					koi.setLinearVelocity(koi.getLinearVelocity().setLength(koi.getLinearVelocity().len()-1));
 //				}
 //				break;
 //			case 2:
-//				fish.setLinearVelocity(fish.getLinearVelocity().setLength(tetherSpeed));
+//				koi.setLinearVelocity(koi.getLinearVelocity().setLength(tetherSpeed));
 //				break;
 //			}
 //		}
@@ -445,8 +441,8 @@ public class FishController extends WorldController implements ContactListener {
 		Body body1 = contact.getFixtureA().getBody();
 		Body body2 = contact.getFixtureB().getBody();
 
-		if( (body1.getUserData() == fish   && body2.getUserData() == goalDoor) ||
-			(body1.getUserData() == goalDoor && body2.getUserData() == fish)) {
+		if( (body1.getUserData() == koi   && body2.getUserData() == goalDoor) ||
+			(body1.getUserData() == goalDoor && body2.getUserData() == koi)) {
 			setComplete(true);
 		}
 	}
@@ -496,10 +492,10 @@ public class FishController extends WorldController implements ContactListener {
 		if (speed > SOUND_THRESHOLD) {
 			String s1 = ((Obstacle)body1.getUserData()).getName();
 			String s2 = ((Obstacle)body2.getUserData()).getName();
-			if (s1.equals("rocket") || s1.startsWith("crate") || s1.startsWith("tether")) {
+			if (s1.equals("koi") || s1.startsWith("enemy") || s1.startsWith("tether")) {
 				SoundController.getInstance().play(s1, COLLISION_SOUND, false, 0.5f);
 			}
-			if (s2.equals("rocket") || s2.startsWith("crate") || s2.startsWith("tether")) {
+			if (s2.equals("koi") || s2.startsWith("enemy") || s2.startsWith("tether")) {
 				SoundController.getInstance().play(s2, COLLISION_SOUND, false, 0.5f);
 			}
 		}
