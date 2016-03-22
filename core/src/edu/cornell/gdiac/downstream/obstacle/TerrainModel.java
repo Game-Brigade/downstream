748107fb58df022cdbb592f8da3e66c679ccddc9
oka2 <oka2@cornell.edu>
2016-03-22 11:18:37 -0400

    adding terrain model

--- core/src/edu/cornell/gdiac/downstream/obstacle/TerrainModel.java
+++ core/src/edu/cornell/gdiac/downstream/obstacle/TerrainModel.java
@@ -1,9 +1,15 @@
 package edu.cornell.gdiac.downstream.obstacle;
 
-
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 
 public class TerrainModel extends BoxObstacle {
 
-	
+	public TerrainModel(float x, float y, float width, float height) {
+		super(x,y);
+		
+		
+		
+	}
 
 }
