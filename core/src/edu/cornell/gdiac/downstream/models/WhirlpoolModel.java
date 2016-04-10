2b60d81c749085f32dd8455d0509f48659a722cd
oka2 <oka2@cornell.edu>
2016-04-10 18:43:13 -0400

    adding whirlpool

---
+++ core/src/edu/cornell/gdiac/downstream/models/WhirlpoolModel.java
@@ -0,0 +1,11 @@
+package edu.cornell.gdiac.downstream.models;
+
+import edu.cornell.gdiac.downstream.obstacle.WheelObstacle;
+
+public class WhirlpoolModel extends WheelObstacle {
+
+	public WhirlpoolModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
